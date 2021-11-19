8a4942b54e9cb05ae3ef1193269f4e898a229ee5
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2021-11-19 10:02:30 +0100

    update visualization

--- arp_vis.py
+++ arp_vis.py
@@ -3,8 +3,10 @@ from astropy.time import Time
 import numpy as np
 
 from poliastro.plotting import StaticOrbitPlotter
+from poliastro.plotting.util import generate_label
 import matplotlib.pyplot as plt
-
+from poliastro.twobody.propagation import propagate
+from astropy.time import TimeDelta
 from arp import AsteroidRoutingProblem
 from space_util import (
     Asteroids,
@@ -23,22 +25,27 @@ def plot_solution(self, x):
     epoch = START_EPOCH
     ship = Earth.propagate(epoch)
     frame.plot(ship, label="Earth")
+        
     for k, (ast, man) in enumerate(zip(x,sol.ship.maneuvers)):
         print(k)
         print(ast)
         print(man)
         epoch += t[2*k]
         ship = ship.propagate(epoch)
-        frame.plot_maneuver(ship, man, label=f'Impulse {ast}')
+        frame.plot_maneuver(ship, man, color = f'C{k+1}', label=generate_label(epoch, f'Impulse {k}'))
         ship = ship.apply_maneuver(man)
-        # frame.plot_trajectory(
-        #    ship.sample(max_anomaly=180 * u.deg), label=f'Asteroid {ast}')
-        #frame.plot(ship, label=f'Asteroid {ast}')
-        
+        epoch += t[2*k + 1]
+        if 2*(k+1) >= len(t):
+            tofs = TimeDelta(0 * u.day)
+        else:
+            tofs = TimeDelta(np.linspace(0,  t[2*(k+1)] * u.day, num=100))
+        rr = propagate(ship, tofs)
+        frame.plot_trajectory(rr, color = f'C{k+1}',label=generate_label(epoch, f'Asteroid {ast}'))
+          
     plt.show()
 
 
-instance = AsteroidRoutingProblem(4, 42)
+instance = AsteroidRoutingProblem(10, 42)
 x,f = instance.nearest_neighbor([], "euclidean")
 print(x)
 print(f)
