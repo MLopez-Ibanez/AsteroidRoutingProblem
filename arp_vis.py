9ec2f276bc664597a6c36f72bb172273cba47b8e
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2021-11-24 13:52:55 +0100

    update plots and tables

--- arp_vis.py
+++ arp_vis.py
@@ -12,11 +12,51 @@ from space_util import (
     Earth,
 )
 
-def plot_solution(self, x):
+# From https://jwalton.info/Embed-Publication-Matplotlib-Latex/
+def get_fig_size(width, fraction=1, subplots=(1, 1)):
+    """Get figure dimensions to avoid scaling in LaTeX.
+
+    Parameters
+    ----------
+    width: float or string
+            Document width in points, or string of predined document type
+    fraction: float, optional
+            Fraction of the width which you wish the figure to occupy
+    subplots: array-like, optional
+            The number of rows and columns of subplots.
+    Returns
+    -------
+    fig_dim: tuple
+            Dimensions of figure in inches
+    """
+    if width == 'thesis':
+        width_pt = 426.79135
+    elif width == 'beamer':
+        width_pt = 307.28987
+    elif width == 'lncs':
+        width_pt = 347.12354
+    else:
+        width_pt = width
+
+    # Width of figure (in pts)
+    fig_width_pt = width_pt * fraction
+    # Convert from pt to inches
+    inches_per_pt = 1 / 72.27
+    # Golden ratio to set aesthetic figure height
+    # https://disq.us/p/2940ij3
+    golden_ratio = (5**.5 - 1) / 2
+    # Figure width in inches
+    fig_width_in = fig_width_pt * inches_per_pt
+    # Figure height in inches
+    fig_height_in = fig_width_in * golden_ratio * (subplots[0] / subplots[1])
+    return (fig_width_in, fig_height_in)
+
+
+def plot_solution(self, x, ax = None):
     sol = self.CompleteSolution(x)
     t = sol.ship.x
     print(t)
-    frame = StaticOrbitPlotter(plane=Earth.plane)
+    frame = StaticOrbitPlotter(ax = ax, plane=Earth.plane)
     epoch = START_EPOCH
     ship = Earth.propagate(epoch)
     frame.plot(ship, label="Earth")
@@ -37,7 +77,7 @@ def plot_solution(self, x):
         rr = propagate(ship, tofs)
         frame.plot_trajectory(rr, color = f'C{k+1}',label=generate_label(epoch, f'Asteroid {ast}'))
     
-    return frame
+    return ax, frame
 
 
 
