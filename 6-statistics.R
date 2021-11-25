99d0c01a96980136646bcbb338a9a8fb0f970c09
Francisco Chicano <chicano@lcc.uma.es>
2021-11-25 08:33:59 +0100

    stats

--- 6-statistics.R
+++ 6-statistics.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(xtable)
 read.data <- function (folder, instance, er, alg, distance) {
   subfolder <- paste('m400-er',er,sep='')
   path <- paste(folder, subfolder, instance, sep='/')
@@ -21,8 +22,10 @@ for (s in c(10,15,20,25,30)) {
 }
 
 compute.bb.stats <- function() {
-  result <- data.frame(instance=character(), cego.bb.repr=numeric(), umm.bb.repr=numeric(), 
-                       ranking.bb.alg=numeric(), order.bb.alg=numeric())
+  result <- data.frame(instance=character(), 
+                       cego.bb.repr=numeric(), 
+                       umm.bb.repr=numeric(), 
+                       best.bb.alg=numeric())
   
   # Ranking is er=0, Order is er=1
   # FIXME: not sure about the above
@@ -34,32 +37,35 @@ compute.bb.stats <- function() {
     
     cego.bb.repr <- wilcox.test(get.end.fitness(cego_ranking), get.end.fitness(cego_order))$p.value
     umm.bb.repr <- wilcox.test(get.end.fitness(umm_ranking), get.end.fitness(umm_order))$p.value
-    ranking.bb.alg <- wilcox.test(get.end.fitness(cego_ranking), get.end.fitness(umm_ranking))$p.value
-    order.bb.alg <- wilcox.test(get.end.fitness(cego_order), get.end.fitness(umm_order))$p.value
+    best.bb.alg <- wilcox.test(get.end.fitness(cego_order), get.end.fitness(umm_ranking))$p.value
     
-    result[nrow(result)+1,] = c(instances[i], cego.bb.repr, umm.bb.repr, ranking.bb.alg, order.bb.alg)
-    
+    result[i,1] = instances[i]
+    result[i,2] = cego.bb.repr
+    result[i,3] = umm.bb.repr 
+    result[i,4] =  best.bb.alg
   }
-  
+  print(xtable(result,display=c("d","s","e","e","e")), include.rownames=FALSE)
   return (result) 
 }
 
 compute.informed.stats <- function() {
-  result <- data.frame(instance=character(), umm.order.greedy=numeric(), cego.rank.greedy=numeric())
+  result <- data.frame(instance=character(), umm.rank.greedy=numeric(), cego.order.greedy=numeric())
   
   # Ranking is er=0, Order is er=1
   for (i in 1:length(instances)) {
-    cego_bb <- read.data('results', instances[i], 0, 'cego', 'maxmindist')
-    cego_greedy <- read.data('results', instances[i], 0, 'cego', 'greedy_euclidean')
-    umm_bb <- read.data('results', instances[i], 1, 'umm', 'maxmindist')
-    umm_greedy <- read.data('results', instances[i], 1, 'umm', 'greedy_euclidean')
+    cego_bb <- read.data('results', instances[i], 1, 'cego', 'maxmindist')
+    cego_greedy <- read.data('results', instances[i], 1, 'cego', 'greedy_euclidean')
+    umm_bb <- read.data('results', instances[i], 0, 'umm', 'maxmindist')
+    umm_greedy <- read.data('results', instances[i], 0, 'umm', 'greedy_euclidean')
     
-    umm.order.greedy <- wilcox.test(get.end.fitness(umm_bb), get.end.fitness(umm_greedy))$p.value
-    cego.rank.greedy <- wilcox.test(get.end.fitness(cego_bb), get.end.fitness(cego_greedy))$p.value
-    
-    result[nrow(result)+1,] = c(instances[i], umm.order.greedy, cego.rank.greedy)
-    
+    umm.rank.greedy <- wilcox.test(get.end.fitness(umm_bb), get.end.fitness(umm_greedy))$p.value
+    cego.order.greedy <- wilcox.test(get.end.fitness(cego_bb), get.end.fitness(cego_greedy))$p.value
+    #print(is.numeric(umm.rank.greedy))
+    result[i,1] = instances[i]
+    result[i,2] = umm.rank.greedy
+    result[i,3] = cego.order.greedy
   }
+  print(xtable(result,display=c("d","s","e","e")), include.rownames=FALSE)
   
   return (result) 
 }
