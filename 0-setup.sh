4b9de0c72f07acce0d6f3e9039d1aef4906c9bde
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2025-03-28 09:54:02 +0000

    * 0-setup.sh: Use remotes package.

--- 0-setup.sh
+++ 0-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-Rscript -e 'install.packages(c("remotes", "quadprog"), repos="https://cloud.r-project.org/");remotes::install_version("CEGO", version = "2.4.2")'
+Rscript -e 'install.packages(c("remotes", "quadprog"), repos="https://cloud.r-project.org/");remotes::install_version("CEGO", version = "2.4.2", upgrade="never")'
 python3 -m pip install -r python-requirements.txt
