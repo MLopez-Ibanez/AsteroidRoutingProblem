04e0965a05f57247feb126ba60f2151b2414da77
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2026-04-22 11:48:59 +0100

    0-setup.h: Install quadprog.

--- 0-setup.sh
+++ 0-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-Rscript -e 'install.packages("devtools", repos="https://cloud.r-project.org/");devtools::install_version("CEGO", version = "2.4.2")'
+Rscript -e 'install.packages(c("remotes", "quadprog"), repos="https://cloud.r-project.org/");remotes::install_version("CEGO", version = "2.4.2")'
 python3 -m pip install -r python-requirements.txt
