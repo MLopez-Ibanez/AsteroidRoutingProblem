638ac4b64cfeb26be7a552c3033d62d66d8a77b9
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2026-04-22 11:51:11 +0100

    arp_vis.py: Improve look.

--- arp_vis.py
+++ arp_vis.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from poliastro.plotting import StaticOrbitPlotter
 from poliastro.plotting.util import generate_label
 from poliastro.twobody.propagation import propagate
+from poliastro.ephem import Ephem
 from astropy import units as u
 from astropy.time import TimeDelta
 from arp import AsteroidRoutingProblem
@@ -68,15 +69,17 @@ def plot_solution(self, x, ax = None):
         print(man)
         epoch += t[2*k]
         ship = ship.propagate(epoch)
-        frame.plot_maneuver(ship, man, color = f'C{k+1}', label=generate_label(epoch, f'Impulse {k}'))
+        frame.plot_maneuver(ship, man, color = f'C{k}', label=generate_label(epoch, f'Transfer {k+1}'))
         ship = ship.apply_maneuver(man)
         epoch += t[2*k + 1]
         if 2*(k+1) >= len(t):
-            tofs = TimeDelta(0 * u.day)
+#            tofs = TimeDelta(np.array([0]) * u.day)
+            #rr = propagate(ship, tofs)
+            frame.plot_ephem(Ephem.from_orbit(ship, ship.epoch, plane=Earth.plane), epoch = ship.epoch, color = f'C{k+1}',label=generate_label(epoch, f'Asteroid {ast}'))
         else:
             tofs = TimeDelta(np.linspace(0,  t[2*(k+1)] * u.day, num=100))
-        rr = propagate(ship, tofs)
-        frame.plot_trajectory(rr, color = f'C{k+1}',label=generate_label(epoch, f'Asteroid {ast}'))
+            rr = propagate(ship, tofs)
+            frame.plot_trajectory(rr, color = f'C{k+1}',label=generate_label(epoch, f'Asteroid {ast}'))
 
     return ax, frame, sol.f, sol.get_cost(), sol.get_time()
 
@@ -84,10 +87,28 @@ def plot_solution_to_pdf(instance, sol, pdf_file, title = None, figsize = "lncs"
     fig, ax = plt.subplots(figsize=get_fig_size(figsize, fraction=1))
     ax, _, f, cost, time = plot_solution(instance, sol, ax = ax)
     if title is not None:
-        fig.suptitle(title + f'$\Delta v$={cost:.1f} km/s, $T$={time:.1f} days, $f$={f:.1f}', x=.58, y=0.94)
+        fig.suptitle(title + f'$\Delta V$={cost:.1f} km/s, $T$={time:.1f} days, $f(\pi)$={f:.1f}', x=.58, y=0.94)
     fig.savefig(pdf_file, bbox_inches="tight")
 
-# instance = AsteroidRoutingProblem(10, 42)
+instance = AsteroidRoutingProblem(10, 8)
+
+tex_fonts = {
+    # Use LaTeX to write all text
+    "text.usetex": True,
+    "font.family": "serif",
+    # Use 10pt font in plots, to match 10pt font in document
+    "axes.labelsize": 9,
+    "font.size": 8,
+    # Make the legend/label fonts a little smaller
+    "legend.fontsize": 6,
+    "xtick.labelsize": 8,
+    "ytick.labelsize": 8,
+    # Use Type 1 fonts in plots.
+    "pdf.fonttype": 42,
+}
+plt.rcParams.update(tex_fonts)
+plot_solution_to_pdf(instance, [5,3,2,8,1,9,6,7,0,4], pdf_file = "sol_dd_10_8.pdf", figsize="thesis")
+
 # x,f = instance.nearest_neighbor([], "euclidean")
 # print(x)
 # print(f)
